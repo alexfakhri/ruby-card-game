90ad0ee9440eca06476d5b400ab2c39ccd8874f1
Alex Fakhri <alexfakhri@me.com>
2017-05-30 23:12:52 +0100

    Added deck of cards

---
+++ lib/deck.rb
@@ -0,0 +1,17 @@
+class Deck
+
+  CARD_COUNT = 52
+  RANKS = %w{A 2 3 4 5 6 7 8 9 10 J Q K}
+  SUITS = %w{Spades Hearts Diamonds Clubs}
+
+  attr_reader :cards
+
+  def initialize
+      @cards = build_deck
+    end
+
+  def build_deck
+    @cards = RANKS.product(SUITS).shuffle
+  end
+
+end
---
+++ spec/deck_spec.rb
@@ -0,0 +1,17 @@
+require 'deck'
+
+describe Deck do
+
+  it "has an array of ranks" do
+    expect(Deck::RANKS).to eq ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  end
+
+  it "has an array of ranks" do
+    expect(Deck::SUITS).to eq ["Spades", "Hearts", "Diamonds", "Clubs"]
+  end
+
+  it 'initialise with a full deck of cards' do
+    expect(subject.cards.count).to eq Deck::CARD_COUNT
+  end
+
+end
